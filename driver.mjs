4e38121feb5e7471805aa4350f606803b8990a0e
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-04-19 19:59:18 +0800

    Add prettyPrint function; test tree factory methods

--- driver.mjs
+++ driver.mjs
@@ -11,6 +11,73 @@ function createArray(n) {
     return newArray;
 }
 
+ const prettyPrint = (node, prefix = "", isLeft = true) => {
+   if (node === null) {
+     return;
+   }
+   if (node.right !== null) {
+     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+   }
+   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+   if (node.left !== null) {
+     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+   }
+ };
+
+
+// Create array of random number below 100
 const foo = createArray(10);
 
+// Create binary tree object
 let tree = Tree(foo);
+
+prettyPrint(tree.root);
+
+// check if tree is balanced
+console.log("Is Balanced: ", tree.isBalanced());
+
+// print out elements level order traversal
+console.log("Level order: ", tree.levelOrder());
+
+// print out elements in pre order traversal
+console.log("Pre Order: ", tree.preOrder());
+
+// print out elements in inorder traversal
+console.log("Inorder: ", tree.inOrder());
+
+// print out elements in post order traversal
+console.log("Post Order: ", tree.postOrder());
+
+// unbalance the tree by adding number greater than 100
+tree.insert(145);
+tree.insert(354);
+tree.insert(109);
+tree.insert(287);
+tree.insert(201);
+
+console.log("Unbalance tree:");
+prettyPrint(tree.root);
+
+// check if tree is balanced
+console.log("Is Balanced: ", tree.isBalanced());
+
+// rebalance tree
+tree.root = tree.reBalance();
+
+console.log("Rebalance tree:");
+prettyPrint(tree.root);
+
+// check if tree is balanced
+console.log("Is Balanced: ", tree.isBalanced());
+
+// print out elements level order traversal
+console.log("Level order: ", tree.levelOrder());
+
+// print out elements in pre order traversal
+console.log("Pre Order: ", tree.preOrder());
+
+// print out elements in inorder traversal
+console.log("Inorder: ", tree.inOrder());
+
+// print out elements in post order traversal
+console.log("Post Order: ", tree.postOrder());
